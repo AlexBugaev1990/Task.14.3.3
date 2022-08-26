97a0380270c14f5b60d4799f2713e8d1212ad8a9
AlexBugaev1990 <alex.bugaev90@gmail.com>
2022-08-26 17:11:33 +0300

    Добавьте файлы проекта.

---
+++ Task.14.3.3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task._14._3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Петров", 79990000113, "anatolyP@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            // Сортировка по возрасту
+            var sortedStuds = from s in phoneBook orderby s.Name, s.LastName select s;
+
+            // выводим результат
+            Console.WriteLine("Телефонная книга:");
+            foreach (var book in sortedStuds)
+             Console.WriteLine(book.Name + " " + book.LastName + ": " + book.PhoneNumber);
+
+                    Console.WriteLine();
+                
+            
+        }
+    }
+    public class Contact // модель класса
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email) // метод-конструктор
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+    }
+    
+}
